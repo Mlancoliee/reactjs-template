e622c5f5eea7c7bdc49b3a3b35d41af7d073f214
jesperxu <jesperxu@tencent.com>
2025-10-30 19:05:03 +0800

    delete error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,6 @@ import logo from '../logo.svg';
 import '../App.css';
 
 function Home() {
-  const p = {
-    name: 'zs'
-    age: 18,
-  }
   return (
     <div className="App">
       <header className="App-header">
